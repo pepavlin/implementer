238eea026dbafa83f7a14e4de4cf29c8bb319046
Petr Pavlín <pe.pavlin@gmail.com>
2026-02-26 19:58:58 +0100

    feat: update TypeScript configuration for testing support

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,5 +3,8 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     include: ["tests/**/*.test.ts"],
+    typecheck: {
+      tsconfig: "./tsconfig.test.json",
+    },
   },
 });
